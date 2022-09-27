4aaf723256415e83c7ed1c12e29c3a2cb888bd91
Harsh Sharma <sharmaharshy@gmail.com>
2022-09-27 17:49:39 +0530

    addition of group lifecycle APIs

--- back-end/src/controller/group-controller.ts
+++ back-end/src/controller/group-controller.ts
@@ -1,29 +1,106 @@
+import { getRepository } from "typeorm"
 import { NextFunction, Request, Response } from "express"
 
+import { Group } from "../entity/group.entity"
+import { CreateGroupInput, UpdateGroupInput } from "../interface/group.interface"
+
 export class GroupController {
+  private groupRepository = getRepository(Group)
 
   async allGroups(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
-    // Return the list of all groups
+    // Task 1: Return the list of all groups
+    return this.groupRepository.find()
   }
 
   async createGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
-    // Add a Group
+    // Task 1: Add a Group
+    const { body: params } = request
+
+    if(!(params.ltmt === '<' || params.ltmt === '>')) {
+      response.status(400)
+      return {
+        "message": "Field ltmt can only be '<' or '>'"
+      }
+    }
+
+    const validRoleStates = params.roll_states.split(',').every((item) => ['unmark', 'present', 'absent', 'late'].includes(item))
+    if(!validRoleStates) {
+      response.status(400)
+      return {
+        "message": "Field roll_states only allows one or more amongst 'unmark', 'present', 'absent', 'late' values"
+      }
+    }
+
+    const createGroupInput: CreateGroupInput = {
+      name: params.name,
+      number_of_weeks: params.number_of_weeks,
+      roll_states: params.roll_states,
+      incidents: params.incidents,
+      ltmt: params.ltmt,
+      run_at: null,
+      student_count: 0
+    }
+    const group = new Group()
+    group.prepareToCreate(createGroupInput)
+
+    return await this.groupRepository.save(group)
   }
 
   async updateGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
-    // Update a Group
+    // Task 1:  Update a Group
+    const { body: params } = request
+
+    const groupToUpdate = await this.groupRepository.findOne(params.id)
+    if(groupToUpdate) {
+
+      if(!(params.ltmt === '<' || params.ltmt === '>')) {
+        response.status(400)
+        return {
+          "message": "Field ltmt can only be '<' or '>'"
+        }
+      }
+  
+      const validRoleStates = params.roll_states.split(',').every((item) => ['unmark', 'present', 'absent', 'late'].includes(item))
+      if(!validRoleStates) {
+        response.status(400)
+        return {
+          "message": "Field roll_states only allows one or more amongst 'unmark', 'present', 'absent', 'late' values"
+        }
+      }
+
+      const updateGroupInput: UpdateGroupInput = {
+        id: params.id,
+        name: params.name,
+        number_of_weeks: params.number_of_weeks,
+        roll_states: params.roll_states,
+        incidents: params.incidents,
+        ltmt: params.ltmt,
+        run_at: params.run_at,
+        student_count: params.student_count
+      }
+      groupToUpdate.prepareToUpdate(updateGroupInput)
+      return this.groupRepository.save(groupToUpdate)
+    } else {
+      response.status(400)
+      return {
+        "message": `Group with ID '${params.id}' not found.`
+      }
+    }
   }
 
   async removeGroup(request: Request, response: Response, next: NextFunction) {
-    // Task 1: 
-    
-    // Delete a Group
+    // Task 1: Delete a Group
+    const { params: params } = request
+
+    const groupToRemove = await this.groupRepository.findOne(params.id)
+    if(groupToRemove) {
+      return this.groupRepository.remove(groupToRemove)
+    } else {
+      response.status(400)
+      return {
+        "message": `Group with ID '${params.id}' not found.`
+      }
+    }
   }
 
   async getGroupStudents(request: Request, response: Response, next: NextFunction) {
